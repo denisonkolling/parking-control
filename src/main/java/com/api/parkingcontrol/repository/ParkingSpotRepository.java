dfd6936e9bc1be870029031654b12415a22ded02
Denison Kolling <denisonkolling@gmail.com>
2023-09-28 15:42:05 -0300

    feat: add validations to parking spot

--- src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
@@ -6,4 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicenseCarPlate(String licenseCarPlate);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+
+
 }
--- src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
@@ -4,6 +4,7 @@ import com.api.parkingcontrol.model.ParkingSpotModel;
 import com.api.parkingcontrol.repository.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ParkingSpotService {
@@ -15,7 +16,20 @@ public class ParkingSpotService {
         this.parkingSpotRepository = parkingSpotRepository;
     }
 
+    @Transactional
     public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
         return parkingSpotRepository.save(parkingSpotModel);
     }
+
+    public boolean existsByLicenseCarPlate(String licenseCarPlate) {
+        return parkingSpotRepository.existsByLicenseCarPlate(licenseCarPlate);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber){
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block){
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    };
 }
